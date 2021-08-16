59a4319c2ece7709e911f839f688f2c70ac25fae
Felipe Guimaraes <felipeguima4@gmail.com>
2021-08-16 16:44:27 -0300

    adds chaveamento test

--- tests/TabelaDeChaveamentoTest.java
+++ tests/TabelaDeChaveamentoTest.java
@@ -7,22 +7,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TabelaDeChaveamentoTest {
     @Test
-    public void testShouldSetJogosDasOitavas() {
+    public void itShouldValidateTheSemifinalWinners() {
         TabelaDeChaveamento tabela = new TabelaDeChaveamento();
 
-        List<Jogo> oitavas = new ArrayList<>();
+        List<Jogo> semi = new ArrayList<>();
         Equipe alemanha = new Equipe("Alemanha", "Hans-Dieter Flick");
         Equipe argentina = new Equipe("Argentina", "Lionel Scaloni");
         Equipe brasil = new Equipe("Brasil", "Tite");
         Equipe franca = new Equipe("França", "Didier Deschamps");
 
-        Jogo jogo1 = new Jogo();
-        Jogo jogo2 = new Jogo();
+        Jogo jogo1 = new Jogo(alemanha, franca, 3, 2);
+        Jogo jogo2 = new Jogo(brasil, argentina, 5, 0);
 
-        oitavas.add(jogo1);
-        oitavas.add(jogo2);
-        tabela.setOitavas(oitavas);
+        semi.add(jogo1);
+        semi.add(jogo2);
+        tabela.setSemifinal(semi);
 
-        assertEquals(tabela.getOitavas(), oitavas);
+        assertEquals(tabela.getSemifinal().get(0).getVencedor().getNome(), alemanha.getNome());
+        assertEquals(tabela.getSemifinal().get(1).getVencedor().getNome(), brasil.getNome());
     }
 }

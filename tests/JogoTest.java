e5de1d5be6d2c5400b113dbde46f9f96f83f3999
Felipe Guimaraes <felipeguima4@gmail.com>
2021-08-18 16:36:04 -0300

    adds jogo test

--- tests/JogoTest.java
+++ tests/JogoTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class JogoTest {
@@ -22,6 +24,30 @@ public class JogoTest {
         Jogo brasilVsArgentina = new Jogo(brasil, argentina, 3, 3);
 
         assertNull(brasilVsArgentina.getVencedor());
+    }
+
+    @Test
+    public void itShouldTestGettersAndSetters(){
+        Equipe brasil = new Equipe("Brasil", "Tite");
+        Equipe argentina = new Equipe("Argentina", "Lionel Scaloni");
+        Estadio estadio = new Estadio("Lusail", "Lusail",80000,
+                "Em construção. Será sede da abertura e encerramento da Copa do Mundo.");
+        LocalDate localDate = LocalDate.now();
+
+        Jogo brasilVsArgentina = new Jogo();
+        brasilVsArgentina.setEquipe1(brasil);
+        brasilVsArgentina.setEquipe2(argentina);
+        brasilVsArgentina.setDataHora(localDate);
+        brasilVsArgentina.setLocal(estadio);
+        brasilVsArgentina.setGolsEquipe1(5);
+        brasilVsArgentina.setGolsEquipe2(4);
+
+        assertEquals(brasilVsArgentina.getEquipe1().getNome(), brasil.getNome());
+        assertEquals(brasilVsArgentina.getEquipe2().getNome(), argentina.getNome());
+        assertEquals(brasilVsArgentina.getDataHora(), localDate);
+        assertEquals(brasilVsArgentina.getLocal().getNome(), estadio.getNome());
+        assertEquals(brasilVsArgentina.getGolsEquipe1(), 5);
+        assertEquals(brasilVsArgentina.getGolsEquipe2(), 4);
     }
 
 }
